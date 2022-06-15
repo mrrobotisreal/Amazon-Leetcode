df168efb3b358a1cc7e15c858faec1bdfa6b75c0
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-15 14:02:06 -0600

    Finished hex version of color changer.

--- DOMPractice/hex.ts
+++ DOMPractice/hex.ts
@@ -48,28 +48,18 @@ button.setAttribute('id', 'btn');
 button.textContent = 'click me';
 containerDiv.appendChild(button);
 
-const colors:string[] = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+
+
+const hex:(number|string)[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
 btn.addEventListener('click', function() {
-  const randomNumber = Math.floor(Math.random() * colors.length);
-  console.log('number -> ', randomNumber);
-  document.body.style.backgroundColor = colors[randomNumber];
-  color.textContent = colors[randomNumber];
+  let hexColor:string = '#';
+  for (let i:number = 0; i < 6; i++) {
+    let num:number = Math.floor(Math.random() * hex.length);
+    hexColor += num;
+    document.querySelector('body').style.backgroundColor = hexColor;
+    colorSpan.textContent = hexColor;
+  }
 });
-
-
-
-
-
-// const colorButton = document.createElement('button');
-// app.appendChild(colorButton);
-// app.style.display = 'flex';
-// app.style.justifyContent = 'center';
-// app.style.alignItems = 'center';
-// app.style.flexDirection = 'column';
-
-// const colorText = document.createElement('h3');
-// colorText.textContent = 'You picked color: ';
-// colorButton.prepend(colorText);
