0def40e20bd32270bb25737ab9f4775238ddcb6f
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-17 17:44:05 -0600

    Added to app.ts. Created app div, set it's id, created input form, input text, and button submit, and finally added the divs that will make up the main tab body.

--- DOMPractice/TabWidget/app.ts
+++ DOMPractice/TabWidget/app.ts
@@ -1,1 +1,27 @@
-const app = document.getElementById('app');
+const body = document.querySelector('body');
+
+const app = document.createElement('div');
+app.setAttribute('id', 'app');
+
+const inputForm = document.createElement('form');
+inputForm.setAttribute('class', 'input-form');
+
+const inputText = document.createElement('input');
+// inputText.setAttribute('id', 'input-text');
+inputText.type = 'text';
+inputText.placeholder = 'Tab Name';
+let inputName = '';
+inputText.addEventListener('change', function(e) {
+  const target = e.target as HTMLInputElement;
+  console.log('Target value is -> ', target.value);
+  inputName = target.value;
+});
+
+const inputSubmit = document.createElement('button');
+inputSubmit.addEventListener('click', function(e) {
+  // Create new Tab with inputName as title
+});
+
+const tabDiv = document.createElement('div');
+const tabHeaderDiv = document.createElement('div');
+const tabBody = document.createElement('div');
