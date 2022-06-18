6337301cf435ed930147838abe5c486c70387616
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-18 13:15:05 -0600

    Finished tab widget. Need to try and do it faster next time.

--- DOMPractice/TabWidget/app.ts
+++ DOMPractice/TabWidget/app.ts
@@ -1,12 +1,14 @@
 /* Needs http server to run, I used python's http server with 'python3 -m http.server' */
 
-import { HeadersReceivedResponse } from "electron";
-
 const body = document.querySelector('body');
 
+let windowHeight:string = '' + window.innerHeight;
+let windowWidth:string = '' + window.innerWidth;
 const app = document.createElement('div');
-body.appendChild(app);
+body.prepend(app);
 app.setAttribute('id', 'app');
+app.style.height = windowHeight;
+app.style.width = windowWidth;
 
 const inputForm = document.createElement('form');
 app.append(inputForm);
@@ -35,47 +37,58 @@ class Tab {
 buttonSubmit.addEventListener('click', function(e) {
   // Create new Tab with inputName as title
   e.preventDefault();
-  const inputText = document.getElementById('inputText') as HTMLInputElement;
-  inputText.value = '';
+  if (tabs.length > 0) {
+    for (let i:number = 0; i < tabs.length; i++) {
+      const eachTab = document.getElementById(tabs[i].tabId) as HTMLImageElement;
+      eachTab.style.display = 'none';
+      const eachHeader = document.getElementById(tabs[i].headerId) as HTMLButtonElement;
+      eachHeader.style.color = 'white';
+    }
+  }
   const tab = new Tab();
   tab.headerId = inputName;
   tab.tabId = `${inputName}-img`;
   tabs.push(tab);
-  if (tabs.length > 0) {
-    for (let i:number = 0; i < tabs.length; i++) {
-      document.getElementById(tabs[i].tabId).style.display = 'none';
-    }
-  }
   fetch('https://dog.ceo/api/breeds/image/random')
     .then((res) => res.json())
     .then((data) => {
       const img = document.createElement('img');
       tabBody.appendChild(img);
-      img.setAttribute('id', `${inputName}-img`);
+      img.setAttribute('id', tab.tabId);
       img.src = `${data.message}`
       img.style.display = 'flex';
     })
     .catch((err) => console.error(err));
+  addHeader(inputName);
+  const inputText = document.getElementById('inputText') as HTMLInputElement;
+  inputText.value = '';
+  inputName = '';
+});
+
+function addHeader(id: string): void {
   const newTabHeader = document.createElement('button');
+  const tabHeader = document.getElementById('tabHeader') as HTMLDivElement;
   tabHeader.appendChild(newTabHeader);
-  newTabHeader.setAttribute('id', inputName);
+  newTabHeader.setAttribute('id', id);
   newTabHeader.setAttribute('class', 'btn');
   newTabHeader.textContent = inputName;
+  newTabHeader.style.color = 'green';
   newTabHeader.addEventListener('click', function(e) {
     console.log('clicked!');
     e.preventDefault();
     const target = e.target as HTMLButtonElement;
     console.log('target id is -> ', target.id);
     for (let i:number = 0; i < tabs.length; i++) {
-      if (tabs[i].tabId === target.id) {
+      if (tabs[i].headerId === target.id) {
         document.getElementById(tabs[i].tabId).style.display = 'flex';
+        document.getElementById(tabs[i].headerId).style.color = 'green';
       } else {
         document.getElementById(tabs[i].tabId).style.display = 'none';
+        document.getElementById(tabs[i].headerId).style.color = 'white';
       }
     }
   });
-  inputName = '';
-});
+};
 
 const tabDiv = document.createElement('div');
 app.append(tabDiv);
