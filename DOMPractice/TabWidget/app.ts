cfdd8ff4fc03e9aa06cc370e4b557f9c9cd39352
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-17 18:41:51 -0600

    Got most of the code done in app.ts, now I just need to implement switching between the tabs.

--- DOMPractice/TabWidget/app.ts
+++ DOMPractice/TabWidget/app.ts
@@ -1,33 +1,54 @@
+/* Needs http server to run, I used python's http server with 'python3 -m http.server' */
+
 const body = document.querySelector('body');
 
 const app = document.createElement('div');
 app.setAttribute('id', 'app');
 
 const inputForm = document.createElement('form');
-inputForm.setAttribute('class', 'input-form');
+inputForm.setAttribute('class', 'inputForm');
 
 const inputText = document.createElement('input');
-// inputText.setAttribute('id', 'input-text');
+inputText.setAttribute('id', 'inputText');
 inputText.type = 'text';
 inputText.placeholder = 'Tab Name';
 let inputName = '';
-inputText.addEventListener('change', function(e) {
+inputText.addEventListener('input', function(e) {
   const target = e.target as HTMLInputElement;
-  console.log('Target value is -> ', target.value);
   inputName = target.value;
 });
 
 const buttonSubmit = document.createElement('button');
+buttonSubmit.setAttribute('class', 'btn');
+buttonSubmit.textContent = 'Add Tab';
 buttonSubmit.addEventListener('click', function(e) {
   // Create new Tab with inputName as title
+  e.preventDefault();
+  const inputText = document.getElementById('inputText') as HTMLInputElement;
+  inputText.value = '';
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then((res) => res.json())
+    .then((data) => {
+      const img = document.createElement('img');
+      img.setAttribute('id', `${inputName}-img`);
+      img.src = `${data.message}`
+      tabBody.appendChild(img);
+    })
+    .catch((err) => console.error(err));
+  const newTabHeader = document.createElement('button');
+  newTabHeader.setAttribute('id', inputName);
+  newTabHeader.setAttribute('class', 'btn');
+  newTabHeader.textContent = inputName;
+  tabHeader.appendChild(newTabHeader);
+  inputName = '';
 });
 
 const tabDiv = document.createElement('div');
 const tabHeader = document.createElement('div');
 const tabBody = document.createElement('div');
 tabDiv.setAttribute('id', 'tabDiv');
-tabHeader.setAttribute('class', 'tabHeader');
-tabBody.setAttribute('class', 'tabBody');
+tabHeader.setAttribute('id', 'tabHeader');
+tabBody.setAttribute('id', 'tabBody');
 
 /* Appending */
 body.appendChild(app);
