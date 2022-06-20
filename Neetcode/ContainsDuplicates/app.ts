bbea5da0301b90d0fa30d5f8ce06fbcbd5fd0847
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-20 15:46:08 -0600

    Finished contains duplicate neetcode and also completed unit tests.

---
+++ Neetcode/ContainsDuplicates/app.ts
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+ var containsDuplicate = function(nums) {
+  for (let i:number = 0; i < nums.length; i++) {
+      for (let j:number = 0; j < nums.length && j !== i; j++) {
+        if (nums[i] === nums[j]) {
+            return true;
+        }
+      }
+  }
+  return false;
+};
+
+function assertContainsDuplicates(test, solution, description) {
+  let answer:boolean = test === solution;
+  if (answer) {
+    console.log('PASS');
+    console.log(`${description}, and returns ${test}`);
+    return test;
+  } else {
+    console.log('FAIL');
+    console.log(`${description}, and returns ${test}`);
+    return test;
+  }
+}
+
+let successTestWithDuplicates = assertContainsDuplicates(containsDuplicate([1, 2, 3, 4, 5, 5]), true, 'should pass and return true');
+let successTestWithoutDuplicates = assertContainsDuplicates(containsDuplicate([1, 2, 3, 4]), false, 'should pass and return false');
+let failTestWithDuplicates = assertContainsDuplicates(containsDuplicate([3, 3, 4, 5, 6, 6]), false, 'should fail and return true');
+let failTestWithoutDuplicates = assertContainsDuplicates(containsDuplicate([6, 7, 8, 9]), true, 'should fail and return false');
+
+console.log(successTestWithDuplicates); // true
+console.log(successTestWithoutDuplicates); // false
+console.log(failTestWithDuplicates); // true
+console.log(failTestWithoutDuplicates); // false
