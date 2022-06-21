0d450e718d59759c9b350f13c928b4547c9f2781
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-21 11:09:07 -0600

    Added solution for topKFrequent, now I need to test it and make sure it works properly.

--- Neetcode/TopKFrequentElements/app.ts
+++ Neetcode/TopKFrequentElements/app.ts
@@ -1,3 +1,28 @@
 // Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
 function topKFrequent(nums: number[], k: number): number[] {
+  console.time('topKFrequent');
+  let result:number[] = [];
+  interface Frequency {
+    [key:string]: number,
+  }
+  let frequency:Frequency = {};
+
+  for (let i:number = 0; i < nums.length; i++) {
+    if (!frequency[nums[i]]) {
+      frequency[nums[i]] = 1;
+    } else {
+      frequency[nums[i]]++;
+    }
+  }
+
+  for (const key in frequency) {
+    if (frequency[key] >= k) {
+      result.push(Number(key));
+    }
+  }
+
+  console.timeEnd('topKFrequent');
+  return result;
 };
+
+topKFrequent([1,1,1,2,2,3], 2);
