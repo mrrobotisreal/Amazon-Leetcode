49f85e334328ea96fb5419e6429cac12ad49c7ec
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-21 20:10:09 -0600

    Finished SudokuChecker, but it's a horrible solution. It definitely needs to be reworked.

--- Neetcode/SudokuChecker/app.ts
+++ Neetcode/SudokuChecker/app.ts
@@ -11,5 +11,86 @@ Only the filled cells need to be validated according to the mentioned rules.
 */
 
 function isValidSudoku(board: string[][]): boolean {
-  //
+  // create regular expression for matching digits
+  const digits:RegExp = /\d/g;
+  // iterate through each row and check for duplicates with indexing
+  let col1:string[] = [];
+  let col2:string[] = [];
+  let col3:string[] = [];
+  let col4:string[] = [];
+  let col5:string[] = [];
+  let col6:string[] = [];
+  let col7:string[] = [];
+  let col8:string[] = [];
+  let col9:string[] = [];
+  let cols:string[][] = [];
+  cols.push(col1);
+  cols.push(col2);
+  cols.push(col3);
+  cols.push(col4);
+  cols.push(col5);
+  cols.push(col6);
+  cols.push(col7);
+  cols.push(col8);
+  cols.push(col9);
+  for (let i:number = 0; i < board.length; i++) {
+    let nums:string[] = board[i].join('').match(digits);
+    let rowSet = new Set<string>(nums);
+    if (nums.length > rowSet.size) {
+      console.log('FAIL row');
+      return false;
+    }
+    for (let j:number = 0; j < board[i].length; j++) {
+      eval(`col${j + 1}.push(board[i][j])`);
+      // console.log(col1);
+    }
+  }
+  console.log('col1 -> ', col1);
+
+  for (let i:number = 0; i < cols.length; i++) {
+    let nums:string[] = cols[i].join('').match(digits);
+    let rowSet = new Set<string>(nums);
+    if (nums.length > rowSet.size) {
+      console.log('FAIL col');
+      return false;
+    }
+  }
+
+  // recursively check each column
+  return true;
 };
+
+const testBoard = [
+ ["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]];
+
+const failRowBoard = [
+ ["5","3","3",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]];
+
+const failColBoard = [
+ ["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]];
+
+console.log(isValidSudoku(failRowBoard));
