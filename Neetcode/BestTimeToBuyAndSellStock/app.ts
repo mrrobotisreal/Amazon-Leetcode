ecb85c31cd05d8b08e5e4b93b366a462d49e2561
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-26 14:07:04 -0600

    Added and finished BestTimeToBuyAndSellStock, but I had to do so with a tutorial and to be honest, I don't really understand why the guy is doing it this way... I think the way I started to do it makes more sense, so I definitely need to come back and rework this when I have more time so I can get a better solution.

--- Neetcode/BestTimeToBuyAndSellStock/app.ts
+++ Neetcode/BestTimeToBuyAndSellStock/app.ts
@@ -13,31 +13,97 @@
 // Output: 0
 // Explanation: In this case, no transactions are done and the max profit = 0.
 
-function maxProfit(prices: number[]): number {
+function maxProfit(prices: number[]): number{
+  let buy: number = prices[0];
+  prices[0] = 0;
   let profit: number = 0;
-  interface Price {
-    price: number,
-    index: number,
-  }
-  let lowest: Price = {
-    price: Math.max(...prices),
-    index: 0,
-  };
-  let highest: Price = {
-    price: Math.min(...prices),
-    index: 0,
-  }
-  let lowestPrices:number[] = [];
-  let highestPrices:number[] = [];
 
-  for (let i = 0; i < prices.length; i++) {
-    if (prices[i] < lowest.price) {
-      lowest.price = prices[i];
-    }
-    if (prices[i] > highest.price) {
-      highest.price = prices[i];
+  for (let i: number = 0; i < prices.length; i++) {
+    if (buy > prices[i]) {
+      buy = prices[i];
+      prices[i] = 0;
+    } else {
+      profit = Math.max(prices[i] - buy, profit);
     }
   }
 
   return profit;
-};
+}
+
+
+
+
+
+
+
+/* First two attempts */
+
+// function maxProfit(prices: number[]): number {
+//   let profit: number = 0;
+//   interface Price {
+//     price: number,
+//     buyIndex: number,
+//     sellIndex: number,
+//   }
+//   let lowest: Price = {
+//     price: Math.max(...prices),
+//     buyIndex: 0,
+//     sellIndex: 0,
+//   };
+//   let highest: Price = {
+//     price: Math.min(...prices),
+//     buyIndex: 0,
+//     sellIndex: 0,
+//   }
+//   let highestProfits: number[] = [];
+//   let profits: number[][] = [];
+
+//   for (let i: number = 0; i < prices.length; i++) {
+//     let currentProfits: number[] = [];
+//     for (let j: number = 0; j < prices.length; j++) {
+//       let greatest: Price = {
+//         price: null,
+//         buyIndex: null,
+//         sellIndex: null,
+//       };
+//       if (j === i || j < i) {
+//         continue;
+//       }
+//       greatest.price
+//       // currentProfits.push(prices[j] - prices[i]);
+//     }
+//     profits.push(currentProfits);
+//     highestProfits.push(Math.max(...currentProfits));
+//   }
+
+//   return profit;
+// };
+
+// function maxProfit(prices: number[]): number {
+//   let profit: number = 0;
+//   interface Price {
+//     price: number,
+//     index: number,
+//   }
+//   let lowest: Price = {
+//     price: Math.max(...prices),
+//     index: 0,
+//   };
+//   let highest: Price = {
+//     price: Math.min(...prices),
+//     index: 0,
+//   }
+//   let lowestPrices:number[] = [];
+//   let highestPrices:number[] = [];
+
+//   for (let i = 0; i < prices.length; i++) {
+//     if (prices[i] < lowest.price) {
+//       lowest.price = prices[i];
+//     }
+//     if (prices[i] > highest.price) {
+//       highest.price = prices[i];
+//     }
+//   }
+
+//   return profit;
+// };
